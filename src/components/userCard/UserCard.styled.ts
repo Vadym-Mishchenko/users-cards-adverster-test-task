fc1a857ffa072aaa6dbe1ba91a03e54fc6aa28b2
Michman <Michman@host-192-168-0-183.sohonet.ua>
2025-05-13 18:45:29 +0300

    motion animation fixed

--- src/components/userCard/UserCard.styled.ts
+++ src/components/userCard/UserCard.styled.ts
@@ -1,22 +1,15 @@
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
-import { Box, Card } from '@mui/material';
+import { Box } from '@mui/material';
 
-export const UserCardStyled = styled(motion(Card))`
+export const UserCardStyled = styled(motion.create(Box))`
   width: 360px;
   max-width: 360px;
   border-radius: 20px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease-in-out;
-  background-color: rgba(255, 255, 255, 0.25);
-  backdrop-filter: blur(50px);
-  -webkit-backdrop-filter: blur(50px);
-  box-shadow: 0 4px 90px rgba(0, 0, 0, 0.1);
   overflow: hidden;
-
-  &:hover {
-    transform: scale(105%);
-  }
+  background-color: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 4px 90px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-in-out;
 
   @media (max-width: 420px) {
     width: 100%;
--- src/components/userCard/UserCard.tsx
+++ src/components/userCard/UserCard.tsx
@@ -64,7 +64,7 @@ export const UserCard = ({ user, index }: UserCardProps) => {
     <UserCardStyled
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.01 }}
+      viewport={{ once: true, amount: 0.2 }}
       transition={{ duration: 0.3, ease: 'easeOut', delay: index * 0.1 }}
     >
       <CardHeader
